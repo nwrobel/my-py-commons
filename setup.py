610668248f5315afdd9ed0bd8427f82b442be29f
Nick Wrobel <nickwrobel2@gmail.com>
2020-08-20 20:33:49 -0400

    Created setup.py to packagize the project so it can be imported by other projects using pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my-python-commons-nwrobel",
+    version="1.0.0",
+    author="Nick Wrobel",
+    author_email="nick@nwrobel.com",
+    description="Package containing common modules that implement frequently needed functionalities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nwrobel/my-python-commons",
+    packages=setuptools.find_packages(),
+    install_requires=[], # use to define external packages to install as well as dependencies to this package
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
