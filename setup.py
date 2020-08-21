267845e13fa94c6970f7d3811e7c4cc8f2015138
Nick Wrobel <nickwrobel2@gmail.com>
2020-08-21 01:44:38 -0400

    updated version number after recent changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="my-python-commons-nwrobel",
-    version="1.0.0",
+    version="1.0.1",
     author="Nick Wrobel",
     author_email="nick@nwrobel.com",
     description="Package containing common modules that implement frequently needed functionalities",
