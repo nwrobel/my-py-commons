2682f1755ac7603c4fb3a49b961a9d3b87c794e4
Nick Wrobel <nickwrobel2@gmail.com>
2020-10-21 05:27:41 -0400

    bug file with the logging module; added several new functions to the file module (related to line counts and removing lines from text file)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="my-python-commons-nwrobel",
-    version="1.0.5",
+    version="1.0.6",
     author="Nick Wrobel",
     author_email="nick@nwrobel.com",
     description="Package containing common modules that implement frequently needed functionalities",
