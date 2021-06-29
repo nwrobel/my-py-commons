eed56d620733dffad99d633615bc50132d8a0002
Nick Wrobel <nickwrobel2@gmail.com>
2021-06-29 18:23:50 -0400

    working on update of all functions in the modules to improve clarity, usefulness, and standardization

--- com/nwrobel/mypycommons/file.py
+++ com/nwrobel/mypycommons/file.py
@@ -18,10 +18,13 @@ import json
 from com.nwrobel import mypycommons
 import com.nwrobel.mypycommons.system
 
-def isValidPossibleFilepath(filepath):
+def isPossibleFilepath(filepath):
     '''
     Checks whether or not the given string is a legal, absolute, valid possible/potential filepath.
     This does not check for the existence of the path.  
+
+    @params
+    filepath: the string to check for a possible filepath
     '''
     return os.path.isabs(filepath)
 
@@ -32,23 +35,32 @@ def getThisScriptCurrentDirectory():
     callerModuleName = _getCallerModuleName()
     return (os.path.dirname(os.path.realpath(callerModuleName)))
 
-def applyPermissionToPath(path, owner, group, mask, onlyChildPathType='', recursive=False):
+def applyPermissionToPath(filepath, owner, group, mask, applyToPathType='', recursive=False):
     '''
     Applies the given Unix file permissions (owner, group, permission mask) to the given path using
     the chown and chmod commands. 
 
-    Note: this requires root (sudo) permissions to work - the python script using this function must
+    @params
+    path: the full path to the file or directory
+    owner: the system username to apply as the owner
+    group: the system groupname to apply as the group
+    applyToPathType: optional, "file" or "dir" to apply permission to only files or only directories
+    recursive: if true, sets the permissions to a directory recursively
+
+    @notes
+    This only works on Linux machines. 
+    This requires root (sudo) permissions to work - the python script using this function must
     be run like "sudo python3 script.py".
     '''
     # Set ownership and permissions using by calling the linux chown and chmod commands
     ownerGroup = "{}:{}".format(owner, group)
 
     if (recursive):    
-        if (onlyChildPathType == 'directory'):
+        if (applyToPathType == 'dir'):
             subprocess.call(['sudo', 'find', path, '-type', 'd', '-exec', 'chown', ownerGroup, '{}', '+'])
             subprocess.call(['sudo', 'find', path, '-type', 'd', '-exec', 'chmod', mask, '{}', '+'])
         
-        elif (onlyChildPathType == 'file'):
+        elif (applyToPathType == 'file'):
             subprocess.call(['sudo', 'find', path, '-type', 'f', '-exec', 'chown', ownerGroup, '{}', '+'])
             subprocess.call(['sudo', 'find', path, '-type', 'f', '-exec', 'chmod', mask, '{}', '+'])
 
@@ -59,60 +71,40 @@ def applyPermissionToPath(path, owner, group, mask, onlyChildPathType='', recurs
     else:
         subprocess.call(['sudo', 'chown', ownerGroup, path])
         subprocess.call(['sudo', 'chmod', mask, path])
-        
-def _getCallerModuleName():
-    '''
-    Returns the name of the caller (of the caller) module. Used by the getThisScriptCurrentDirectory
-    function.
-    '''
-    frm = inspect.stack()[2]
-    module = inspect.getmodule(frm[0])
-    return module.__file__
 
-def GetAllFilesAndDirectoriesRecursive(rootPath, useWindowsExtendedPaths=False, includeRootPath=False):
+def getChildPathsRecursive(rootFilepath, pathType='', useWindowsExtendedPaths=False):
     '''
-    Gets the filepaths of all files AND folders within the given root directory, recursively.
-    
-    All filepaths will use the extended path syntax, to avoid problems with long filepaths, if this parameter 
-    is set.
-    '''
-    rootPathObj = Path(rootPath)
-    children = rootPathObj.glob('**/*')
-    
-    if (useWindowsExtendedPaths):
-        paths = [('\\\\?\\' + str(child)) for child in children]
+    Gets the child paths of the root filepath, recursively.
 
-        if (includeRootPath):
-            paths += [('\\\\?\\' + str(rootPathObj))]
-    else:
-        paths = [str(child) for child in children]
-
-        if (includeRootPath):
-            paths += [str(rootPathObj)]
-
-    return paths
-
-def GetAllFilesRecursive(rootPath, useWindowsExtendedPaths=False):
-    '''
-    Gets the filepaths of all files in the given root directory and all of its subdirectories.
-    Does not return directories. 
-    
-    All filepaths will use the extended path syntax, to avoid problems with long filepaths, if this parameter 
-    is set.
+    @params
+    rootFilepath: the parent filepath (directory) to search for paths within
+    pathType: (optional) "file" or "dir" to return only files or only directories
+    useWindowsExtendedPaths: (optional) makes all paths returned use the Windows extended path 
+        syntax, to avoid problems with long filepaths 
     '''
     pathObj = Path(rootPath)
     childrenObjs = pathObj.glob('**/*')
     
     fileObjs = [childObj for childObj in childrenObjs if _isFile(childObj)]
+    dirObjs = [childObj for childObj in childrenObjs if _isDir(childObj)]
 
     if (useWindowsExtendedPaths):
         filePaths = [('\\\\?\\' + str(fileObj)) for fileObj in fileObjs]
+        dirPaths = [('\\\\?\\' + str(dirObj)) for dirObj in dirObjs]
     else:
         filePaths = [str(fileObj) for fileObj in fileObjs]
+        dirPaths = [str(dirObj) for dirObj in dirObjs]
 
-    return filePaths
+    if (pathType == 'file'):
+        return filePaths
 
-def GetAllFilesByExtension(rootPath, fileExt, useWindowsExtendedPaths=False):
+    elif (pathType == 'dir'):
+        return dirPaths
+    
+    else:
+        return (dirPaths + filePaths)
+
+def getFilesByExtension(rootFilepath, fileExt, useWindowsExtendedPaths=False):
     '''
     Gets the filepaths of all files contained within the given root directory that have the given 
     file extension(s). Searches for files recursively. Either a single file extension or a list of 
@@ -129,7 +121,7 @@ def GetAllFilesByExtension(rootPath, fileExt, useWindowsExtendedPaths=False):
     if (not isinstance(fileExt, list)):
         fileExt = [fileExt]
     
-    allFiles = GetAllFilesRecursive(rootPath, useWindowsExtendedPaths)
+    allFiles = getChildPathsRecursive(rootFilepath, pathType='file', useWindowsExtendedPaths=useWindowsExtendedPaths)
     matchingFilepaths = []
 
     for filepath in allFiles:
@@ -142,43 +134,59 @@ def GetAllFilesByExtension(rootPath, fileExt, useWindowsExtendedPaths=False):
 def createDirectory(folderPath):
     '''
     Creates the directory specified by the given directory path.
+
+    @params
+    folderPath: path of the new directory to create
     '''
     folderPathObject = Path(folderPath)
     folderPathObject.mkdir(parents=True)
 
-def moveFileToDirectory(filepath, targetDirPath):
-    shutil.move(filepath, targetDirPath) 
-
-def CopyFilesToDirectory(srcFiles, destDir):
+def moveFilesToDirectory(filepaths, destDir):
     '''
-    Given a list of source filepaths and a single destination directory path, this copies the files 
-    to that destination. Metadata and permissions are preserved and will be the same as the originals
-    for the new file copies.
-    '''
-    if (not isinstance(srcFiles, list)):
-        srcFiles = [srcFiles]
+    Moves the given files to the destination directory. 
+    Metadata and permissions are preserved.
 
-    for filepath in srcFiles:
+    @params
+    filepaths: list of filepaths to move to the dir
+    destDir: path of the target directory to move the files to
+    '''
+    for filepath in filepaths:
+        shutil.move(filepath, destDir) 
+
+def copyFilesToDirectory(filepaths, destDir):
+    '''
+    Copies the given files to the destination directory. 
+    Metadata and permissions are preserved.
+
+    @params
+    filepaths: list of filepaths to copy to the dir
+    destDir: path of the target directory to copy the file to
+    '''
+    for filepath in filepaths:
         shutil.copy2(filepath, destDir)
 
-def DeleteFile(filePath):
+def deletePath(filepath):
     '''
-    Deletes a single file, given the filepath. Does not delete directories.
-    '''
-    os.remove(filePath)
+    Deletes a single file or directory
 
-def DeleteDirectory(directoryPath):
+    @params
+    filepath: the path of the file or directory to delete
     '''
-    Deletes the given directory and all files/folders contained within it, recursively.
-    '''
-    shutil.rmtree(directoryPath)
+    pathObj = Path(filepath)
+
+    if (_isFile(pathObj)):
+        os.remove(filePath)
+    elif (_isDir(pathObj)):
+        shutil.rmtree(directoryPath)
 
 def getParentDirectory(filepath, useWindowsExtendedPaths=False):
     '''
-    Given a filepath, returns the parent directory of the file or folder object. 
+    Given a path, returns the parent directory. 
 
-    Dir path will use the extended path syntax, to avoid problems with long filepaths, if this parameter 
-    is set.
+    @params
+    path: file or directory path
+    useWindowsExtendedPaths: (optional) makes path returned use the Windows extended path 
+        syntax, to avoid problems with long filepaths 
     '''
     filePathObject = Path(filepath)
 
@@ -189,24 +197,24 @@ def getParentDirectory(filepath, useWindowsExtendedPaths=False):
 
     return parentDir
 
-def GetFilename(filePath):
+def getFilename(filepath):
     '''
-    Given a filepath, returns only the filename, without the parent folders and containing its 
+    Given a filepath, returns only the filename part, without the parent folders and containing its 
     file extension.
     '''
-    filePathObject = Path(filePath)
+    filePathObject = Path(filepath)
     return filePathObject.name
 
-def GetFileExtension(filePath):
+def getFileExtension(filepath):
     '''
     Returns the file extension of a file, given its filepath. Specifically, this returns the final 
     ".something" in the given file's name. File extension is returned including the dot.
     Returns an empty string if no file extension exists.
     '''
-    filePathObject = Path(filePath)
+    filePathObject = Path(filepath)
     return filePathObject.suffix
 
-def GetFileBaseName(filePath):
+def getFileBaseName(filepath):
     '''
     Returns the "base" name of the file, given the filepath. The base name is the filename minus the
     file's extension. 
@@ -214,15 +222,15 @@ def GetFileBaseName(filePath):
     ex) C:\data\playlist.m3u.tar --> playlist.m3u
     ex) C:\prog\connect.log --> connect
     '''
-    filePathObject = Path(filePath)
+    filePathObject = Path(filepath)
     return filePathObject.stem
 
-def JoinPaths(path1, path2):
+def joinPaths(path1, path2):
     '''
     Given a root absolute filepath and a child relative filepath, returns the effective combination
     of these two paths to make a 3rd filepath.
 
-    ex) JoinPaths("C:\prog\temp", "..\test.txt") --> "C:\prog\test.txt" 
+    ex) joinPaths("C:\prog\temp", "..\test.txt") --> "C:\prog\test.txt" 
     '''
     joined = os.path.join(path1, path2)
     return os.path.abspath(joined)
@@ -307,7 +315,7 @@ def writeToFile(filepath, content):
 def readFile(filepath):
     '''
     Reads the data line by line from the given file and returns a list of strings representing each
-    line of the file. Newlines in the file will show up as newline characters each string.
+    line of the file. Newlines in the file will show up as newline characters for each string.
     '''
     with open(filepath, 'r', encoding='utf-8') as infile:
         fileLines = infile.readlines()
@@ -383,5 +391,13 @@ def _isDir(pathObj):
         else:
             return False
 
+def _getCallerModuleName():
+    '''
+    Returns the name of the caller (of the caller) module. Used by the getThisScriptCurrentDirectory
+    function.
+    '''
+    frm = inspect.stack()[2]
+    module = inspect.getmodule(frm[0])
+    return module.__file__
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+#from com.nwrobel import mypycommons
+import com.nwrobel.mypycommons.misc
+import com.nwrobel.mypycommons.system
+
+print(com.nwrobel.mypycommons.misc.stringStartsWith("this is a great day", "this "))
+print(com.nwrobel.mypycommons.system.thisMachineIsWindowsOS())
